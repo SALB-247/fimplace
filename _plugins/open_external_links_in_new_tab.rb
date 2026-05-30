f0d2bc9b3e51d80c400e0854c80cad17054ed6e2
SALB-247 <fim.hlight@gmail.com>
2026-05-30 21:09:03 +0900

    table update

--- _plugins/open_external_links_in_new_tab.rb
+++ _plugins/open_external_links_in_new_tab.rb
@@ -16,13 +16,20 @@ Jekyll::Hooks.register [:pages], :post_convert do |doc|
 end
 
 def convert_links(doc)
-  open_external_links_in_new_tab = !!doc.site.config["open_external_links_in_new_tab"]
+  return unless !!doc.site.config["open_external_links_in_new_tab"]
 
-  if open_external_links_in_new_tab
-    parsed_doc = Nokogiri::HTML::DocumentFragment.parse(doc.content)
-    parsed_doc.css("a:not(.internal-link):not(.footnote):not(.reversefootnote)").each do |link|
-      link.set_attribute('target', '_blank')
-    end
-    doc.content = parsed_doc.inner_html
+  # 사이트 자기 호스트 (자기 사이트 절대 URL 은 외부로 보지 않음)
+  site_url = doc.site.config["url"].to_s.sub(%r{/\z}, '')
+
+  parsed_doc = Nokogiri::HTML::DocumentFragment.parse(doc.content)
+  parsed_doc.css("a:not(.internal-link):not(.footnote):not(.reversefootnote)").each do |link|
+    href = link.get_attribute('href').to_s.strip
+    next if href.empty?
+    # 외부 링크(다른 호스트의 http/https)에만 새 창. 상대/내부 링크(/tag/, /map/, #anchor, mailto 등)는 같은 탭.
+    next unless href =~ %r{\Ahttps?://}i
+    next if site_url != '' && href.start_with?(site_url)
+    link.set_attribute('target', '_blank')
+    link.set_attribute('rel', 'noopener')   # tabnabbing 방지
   end
+  doc.content = parsed_doc.inner_html
 end
